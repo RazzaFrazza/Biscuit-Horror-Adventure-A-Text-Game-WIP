fce7f4f69e684e98f79b00dc273c944bbd8e8217
RazzaFrazza <networkingartist@gmail.com>
2015-05-28 12:29:51 -0700

    The beginning
    This is the beginning of my text game!

---
+++ Biscuitgame.js
@@ -0,0 +1,37 @@
+// Check if the user is ready to play!
+
+confirm("I am ready");
+
+var age = prompt("What's your age");
+
+if(age < 13)
+{
+    console.log("You are young. You can continue to play but I am not responsible for any heart attacks");
+    
+}
+
+else 
+{
+    console.log("Awesome. You are perfect. mwuahahahaha.");
+    
+}
+
+console.log("You are in an older style house and you hear where are my biscuits'");
+
+console.log("Suddenly you hear the noises of a cane and walker banging against objects.'");
+
+var userAnswer = prompt("What do you do?");
+
+var feedback = prompt("What do you rate my game");
+
+if (feedback > 8)
+{
+    console.log("Thank you! I am glad you enjoy this horror adventure");
+    
+}
+
+else (feedback < 8)
+
+{
+    console.log("I'll keep extra biscuits");
+}
